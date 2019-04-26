db3a8ad0aae83de6acbfc8555e079c8494f26244
Lucas Domingos Leão Gomes <lgomes@dannemann.com.br>
2019-04-25 22:00:57 -0300

    Criando Componente

---
+++ segunda_app_ibd185_mineda/src/components/MainPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <p> Ce acredita? </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainPage',
+  props: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
